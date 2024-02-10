482c182e8df1f2666d555a68994a7b01387d3ae4
Vineethkillamsetty <132868869+Vineethkillamsetty@users.noreply.github.com>
2024-02-10 11:49:39 +0530

    Add files via upload

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const CardItem = props => {
+  const {CardItem} = props
+  const {title, description, imgUrl, className} = CardItem
+  return (
+    <l1 className={`${className}`}>
+      <div>
+        <h1 className="heading-card">{title}</h1>
+        <p className="para-card">{description}</p>
+        <img src={imgurl} className="pic_size" alt="title" />
+      </div>
+    </l1>
+  )
+}
+
+export default CardItem
